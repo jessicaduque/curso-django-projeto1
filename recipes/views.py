a5356697f48a862b5f77c1facd5c164f94e83f60
jduquedeveloper@gmail.com <jduquedeveloper@gmail.com>
2024-09-29 14:08:43 -0300

    Pages and partials

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'recipes/home.html')
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
-
-def sobre(request):
-    return HttpResponse('Sobre!')
+    return render(request, 'recipes/pages/home.html')
