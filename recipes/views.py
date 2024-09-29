ac6d48562ad82395ac31c6f69c392e19d570c1ed
jduquedeveloper@gmail.com <jduquedeveloper@gmail.com>
2024-09-29 13:13:11 -0300

    Templates

--- recipes/views.py
+++ recipes/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('Home!')
+    return render(request, 'recipes/home.html')
 
 def contato(request):
-    return HttpResponse('Contato!')
+    return render(request, 'recipes/contato.html')
 
 def sobre(request):
     return HttpResponse('Sobre!')
